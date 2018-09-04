7f745137d917169d7233bc8e686ed5bda71da8cc
GeniusK <geniusk1047@naver.com>
2018-09-04 13:01:08 +0900

    first commit

---
+++ FirstIphoneApp/FirstIphoneApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirstIphoneApp
+//
+//  Created by 김영재 on 2018. 9. 4..
+//  Copyright © 2018년 GeniusK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
